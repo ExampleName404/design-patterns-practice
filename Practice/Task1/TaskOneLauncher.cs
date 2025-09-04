a3d7cc453c6b815dc1e52abd7f05d304d14a1134
Dmitriy <135806057+ExampleName404@users.noreply.github.com>
2025-09-04 23:29:07 +0500

    Refactor entry point to use TaskOneLauncher
    Moved car initialization and logic from Program.cs to TaskOneLauncher. TaskOneLauncher is now public and uses a Start method instead of a static Main, improving separation of concerns and code organization.

--- Practice/Task1/TaskOneLauncher.cs
+++ Practice/Task1/TaskOneLauncher.cs
@@ -1,6 +1,6 @@
-internal class TaskOneLauncher
+public class TaskOneLauncher
 {
-    public static void Main(string[] args)
+    public void Start()
     {
         Car MySummerCar = new Car("Dodge", 2024, DateTime.Now, "Challenger SRT Demon",2,"АКПП");
         Console.WriteLine($"{MySummerCar.Brand}, {MySummerCar.Model}, имба от {MySummerCar.Year}, когда был запрос {MySummerCar.LastDate}, количество дверей: {MySummerCar.AmountOfDoor}, Тип коробки: {MySummerCar.TransmissionType}");
